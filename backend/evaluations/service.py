081f35d4142099ab176f3aa72f95e9f465a28010
vxmtrung <vuxuanmaitrung1821@gmail.com>
2025-03-25 20:27:28 +0700

    update eval for each semester

--- backend/evaluations/service.py
+++ backend/evaluations/service.py
@@ -6,6 +6,7 @@ from django.db.models.functions import Cast
 from django.db.models import IntegerField
 import os
 import json
+from datetime import datetime
 
 class PathEvalForEachLog:
   def __init__(self, fileName, precision, recall, f1):
@@ -55,22 +56,24 @@ def getLearnLogByStudentCodeAndSemester(studentCode, semester):
    return logs
 
 def getRecommendLogs(studentCode):
+  latest_logs = getLatestLogForEachSemester(studentCode)
   folderPath = Path("logs/" + studentCode)
-  print(folderPath.resolve())
   df_dict = {}
 
-  for file in folderPath.glob("*.txt"):
-      with file.open("r", encoding="utf-8") as f:
-        data = json.load(f)
+  for _, info in latest_logs.items():
+    file_path = folderPath / info["file_name"]
+    if file_path.exists():
+      with file_path.open("r", encoding="utf-8") as f:
+          data = json.load(f)
 
-        records = []
-        for ele in data["learning_path"]:
-          semester = ele["semester"]
-          course_codes = {course["course_code"] for course in ele["courses"]}
-          records.append({"semester": semester, "pred_course": course_codes})
-    
-        df_true = pd.DataFrame(records)
-        df_dict[file.stem] = df_true
+          records = []
+          for ele in data["learning_path"]:
+            semester = ele["semester"]
+            course_codes = {course["course_code"] for course in ele["courses"]}
+            records.append({"semester": semester, "pred_course": course_codes})
+      
+          df_true = pd.DataFrame(records)
+          df_dict[file_path.stem] = df_true
 
   return df_dict
 
@@ -80,7 +83,6 @@ def evaluateRecommendationPathForEachStudent(student_code):
   pathEval = []
 
   for name, df_pred_raw in df_pred_dict.items():
-    print(f"\nLearning Recommend từ file: {name}.txt")
     logs = getLearnLogByStudentCodeAndSemester(student_code, df_pred_raw.iloc[0]["semester"])
     df_true = getYTrueDataFrame(logs)
 
@@ -90,8 +92,10 @@ def evaluateRecommendationPathForEachStudent(student_code):
     cur_sem = df_true["semester"].max()
     df_pred = df_pred_raw[df_pred_raw["semester"] <= cur_sem]
     if df_true.empty and df_pred.empty:
-      pathEval.append(PathEvalForEachLog(name, 0, 0, 0))
+      # pathEval.append(PathEvalForEachLog(name, 0, 0, 0))
       continue
+    
+    print(f"\nLearning Recommend từ file: {name}.txt")
 
     df_merge = df_true.merge(df_pred, on='semester', how='outer')
 
@@ -112,7 +116,72 @@ def evaluateRecommendationPathForEachStudent(student_code):
     print(df_merge[['semester', 'Precision', 'Recall', 'F1-score']])
     pathEval.append(PathEvalForEachLog(name, df_merge['Precision'].mean(), df_merge['Recall'].mean(), df_merge['F1-score'].mean()))
 
+  if len(pathEval) == 0:
+    pathEval.append(PathEvalForEachLog("NULL", 0, 0, 0))
+     
   return PathEvalForEachStudent(student_code, pathEval)
+
+def getPathEvaluationForStudent(student_code):
+  df_pred_dict = getRecommendLogs(student_code)
+
+  pathEval = []
+
+  for name, df_pred_raw in df_pred_dict.items():
+    logs = getLearnLogByStudentCodeAndSemester(student_code, df_pred_raw.iloc[0]["semester"])
+    df_true = getYTrueDataFrame(logs)
+
+    df_true["semester"] = df_true["semester"].astype(int)
+    df_pred_raw["semester"] = df_pred_raw["semester"].astype(int)
+
+    cur_sem = df_true["semester"].max()
+    df_pred = df_pred_raw[df_pred_raw["semester"] <= cur_sem]
+    if df_true.empty and df_pred.empty:
+      # pathEval.append(PathEvalForEachLog(name, 0, 0, 0))
+      continue
+    
+    # print(f"\nLearning Recommend từ file: {name}.txt")
+
+    df_merge = df_true.merge(df_pred, on='semester', how='outer')
+
+    df_merge['actual_course'] = df_merge['actual_course'].apply(lambda x: x if isinstance(x, set) else set())
+    df_merge['pred_course'] = df_merge['pred_course'].apply(lambda x: x if isinstance(x, set) else set())
+    
+    # print(df_merge)
+
+    df_merge['correct'] = df_merge.apply(lambda x: len(x['actual_course'] & x['pred_course']), axis=1)
+
+    df_merge['Precision'] = df_merge.apply(lambda x: x['correct'] / len(x['pred_course']) if len(x['pred_course']) > 0 else 0, axis=1)
+    df_merge['Recall'] = df_merge.apply(lambda x: x['correct'] / len(x['actual_course']) if len(x['actual_course']) > 0 else 0, axis=1)
+    df_merge['F1-score'] = df_merge.apply(
+    lambda x: (2 * x['Precision'] * x['Recall']) / (x['Precision'] + x['Recall']) 
+    if (x['Precision'] + x['Recall']) > 0 else 0, 
+    axis=1)
+    # Hiển thị kết quả
+    # print(df_merge[['semester', 'Precision', 'Recall', 'F1-score']])
+    pathEval.append(df_merge[['semester', 'Precision', 'Recall', 'F1-score']])
+
+  if len(pathEval) == 0:
+    return "No data to evaluate for this student"
+
+  res = pd.concat(pathEval).groupby("semester").mean().reset_index()
+  return res
+
+def evalAll():
+  res = []
+  logs_path = Path("logs")
+  ids = [f.name for f in logs_path.iterdir() if f.is_dir()]
+
+
+  for id in ids:
+    tmp = getPathEvaluationForStudent(id)
+    if isinstance(tmp, str):
+       continue
+    res.append(getPathEvaluationForStudent(id))
+
+  res1 = pd.concat(res).groupby("semester").mean().reset_index()
+
+  return res1
+
 
 def evaluateRecommendationPathForAllStudents():
   res = []
@@ -152,7 +221,6 @@ def evaluateCourseChoicesForEachStudent(student_code):
   pathEval = []
 
   for name, df_pred in df_pred_dict.items():
-    print(f"\nLearning Recommend từ file: {name}.txt")
     logs = getLearnLogByStudentCodeAndSemester(student_code, df_pred.iloc[0]["semester"])
     df_true = getYTrueDataFrame(logs)
 
@@ -160,7 +228,7 @@ def evaluateCourseChoicesForEachStudent(student_code):
     df_pred["semester"] = df_pred["semester"].astype(int)
     
     cur_sem = df_true["semester"].max()
-    df_pred_filtered = df_pred
+    df_pred_filtered = df_pred[df_pred["semester"] <= cur_sem]
 
     if df_true.empty:
        all_true_courses = set()
@@ -190,3 +258,25 @@ def evaluateCourseChoicesForEachStudent(student_code):
 
   return PathEvalForEachStudent(student_code, pathEval)
 
+
+def getLatestLogForEachSemester(student_code):
+  folderPath = Path("logs/" + student_code)
+  print(folderPath.resolve())
+
+  latest_files = {}
+
+  for file in folderPath.glob("*.txt"):
+      with file.open("r", encoding="utf-8") as f:
+        data = json.load(f)
+
+      semester = data.get("learning_path")[0]["semester"]
+      _, date_str, hour_str = file.name[:-4].split("_")
+      file_date = datetime.strptime(date_str + hour_str, "%Y%m%d%H%M%S")
+
+      if semester not in latest_files or file_date > latest_files[semester]["date"]:
+        latest_files[semester] = {"date": file_date, "file_name": file.name}
+
+  # for f, info in latest_files.items():
+  #   print(f"Học kỳ {f}, file_name: {info['file_name']}, file_date: {info['date']}")
+  
+  return latest_files
--- backend/evaluations/views.py
+++ backend/evaluations/views.py
@@ -1,13 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .service import evaluateRecommendationPathForSystem
+from .service import evalAll, getPathEvaluationForStudent
 import pandas as pd
+import json
 
 # Create your views here.
 class EvaluationsAPIView(APIView):
-  def get(self, request, *args, **kwargs):
-    # res = evaluateCourseChoicesForEachStudent('2110001')
-    res = evaluateRecommendationPathForSystem()
-
-    return Response(res, status=status.HTTP_200_OK)
+  def get(self, request, student_id=None):
+    if student_id is None:
+      res = evalAll()
+      return Response(json.loads(res.to_json(orient="records")), status=status.HTTP_200_OK)
+    else:
+      res = getPathEvaluationForStudent(student_id)
+      if isinstance(res, str):
+        return Response(res, status=status.HTTP_400_BAD_REQUEST)
+      return Response(json.loads(res.to_json(orient="records")), status=status.HTTP_200_OK)
